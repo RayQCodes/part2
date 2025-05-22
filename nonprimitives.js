7eeb766bfb489275d26dd47815c1332165dde957
RayQCodes <rquan3@gmu.edu>
2025-05-22 15:36:32 -0400

    update

---
+++ nonprimitives.js
@@ -0,0 +1,28 @@
+//Objects Below
+
+// const username = {
+//   "first name": "hitesh",
+//   "last name": "Bhattarai",
+//   isLoggedin: true,
+// };
+
+// username.firstname = "Pragalv";
+// username.lastname = "Bhattarai";
+// const myFirstname = "Pragalv";
+// console.log(username["last name"]);
+// console.log(username.lastname);
+// console.log(typeof username);
+
+//let today = new Date();
+//console.log(today.getDate());
+
+//Arrays bellow
+let anotherUser = ["Pragalv", "Bhattarai", true];
+
+//console.log(anotherUser[0]);
+
+let isValue = "2abc";
+console.log(typeof Number(isValue));
+//treats as both as strings since one is string
+
+console.log(Number(undefined));
