d90c8468abbd8fca7664c90192c5041c77f504de
RayQCodes <rquan3@gmu.edu>
2025-05-24 13:04:03 -0400

    condition challenges

---
+++ conditionchallenges.js
@@ -0,0 +1,51 @@
+//Check if number is greater than another number:
+``;
+// let num1 = 0;
+// let num2 = 0;
+
+// if (num1 > num2) {
+//   console.log("Num1 is greater than Num2");
+// } else if (num1 == num2) {
+//   console.log("both numbers are the same");
+// } else {
+//   console.log("NUm2 is greater than Num1");
+// }
+
+//check if string is equal to anoth3er string:
+
+// string1 = "Pragalv";
+// string2 = "Pragalv";
+
+// if (string1 == string2) {
+//   console.log("string1 is  equal  to string 2");
+// } else {
+//   console.log("these strings are different");
+// }
+
+//checking if a varaibles is a number or not
+
+// let num1 = "pragalv";
+
+// if (typeof num1 === typeof "Number") {
+//   console.log("num1 is  a number");
+// } else {
+//   console.log(" num1 is not a number");
+// }
+
+//check if boolean is true or false
+// let isloggedin = true;
+// let isnotloggedin = false;
+
+// if (isloggedin === true) {
+//   console.log("user is logged in succesfully");
+// } else {
+//   console.log("user is not logged in");
+// }
+
+// let tasks = ["pragalv", 20, "herndon"];
+
+// if (tasks.length === 0) {
+//   console.log("array is  empty");
+// } else {
+//   console.log("array is not empty");
+// }
